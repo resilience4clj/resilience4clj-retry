e4c0e13709a832012bddaca98dca4661a939d7b2
Tiago Luchini <luchini@work.co>
2019-03-29 09:13:10 -0400

    Bump Clojure to 1.10 due to Java 9 incompat. Add basic tests

--- test/core_test.clj
+++ test/core_test.clj
@@ -4,21 +4,16 @@
             [clojure.test :refer :all]
             [clojure.string :as string]))
 
-;; mock for an external call
-(defn ^:private external-call
-  ([n]
-   (external-call n nil))
-  ([n {:keys [fail? wait]}]
-   (when wait
-     (Thread/sleep wait))
-   (if-not fail?
-     (str "Hello " n "!")
-     (throw (ex-info "Couldn't say hello" {:extra-info :here})))))
+;; mock factory for a failing an external call
+(defn ^:private create-hello-works-after
+  [x]
+  (let [a (atom x)]
+    (fn [n]
+      (swap! a dec)
+      (if (< @a 0)
+        (str "Hello " n "!")
+        (throw (ex-info "Couldn't say hello" {:extra-info :here}))))))
 
-(defn ^:private external-call!
-  [a]
-  (Thread/sleep 250)
-  (swap! a inc))
 
 (deftest create-default-retry
   (let [{:keys [max-attempts
@@ -39,3 +34,28 @@
     (is (= 3 max-attempts))
     (is (string/starts-with? (str (type interval-function))
                              "class io.github.resilience4j.retry.IntervalFunction$$Lambda"))))
+
+(deftest works-after-3-tries-2-failures
+  (let [protected (retry/decorate (create-hello-works-after 2)
+                                  (retry/create "my-retry"
+                                                {:max-attempts 3}))]
+    (is (= "Hello Test!"
+           (protected "Test")))))
+
+(deftest works-after-10-tries-5-failures
+  (let [protected (retry/decorate (create-hello-works-after 5)
+                                  (retry/create "my-retry"
+                                                {:max-attempts 10}))]
+    (is (= "Hello Test!"
+           (protected "Test")))))
+
+(deftest fails-after-2-tries-3-failures
+  (let [protected (retry/decorate (create-hello-works-after 3)
+                                  (retry/create "my-retry"
+                                                {:max-attempts 2}))]
+    (is (thrown? clojure.lang.ExceptionInfo (protected "Test")))
+    (try
+      (protected "Test")
+      (catch Throwable e
+        (is (= :here)
+            (-> e ex-data :extra-info))))))
